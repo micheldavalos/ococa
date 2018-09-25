0219259438deee07f530032c0a72db9db43f3831
Michel Davalos <michel.davalos@academicos.udg.mx>
2018-09-25 14:07:46 -0500

    pagina agregada

---
+++ src/pages/conquista/conquista.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ConquistaPage } from './conquista';
+
+@NgModule({
+  declarations: [
+    ConquistaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ConquistaPage),
+  ],
+})
+export class ConquistaPageModule {}
